6d89c197c76ea34ddb1378158a2fc821a6b6e0a4
Vaibhav-Student <vaibhavbeladiya1112@gmail.com>
2025-03-18 14:34:50 +0530

    set padding, container, add decorate OutlinedButton.

--- lib/outlinedbutton.dart
+++ lib/outlinedbutton.dart
@@ -11,9 +11,31 @@ class CustomOutlinedButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-    return OutlinedButton(
-      onPressed: onPressed,
-      child: Text(text),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        alignment: Alignment.center,
+              padding: const EdgeInsets.all(24.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 235, 235, 235),
+                borderRadius: BorderRadius.circular(12),
+            ),
+        child: SizedBox(
+          width: 300,
+          height: 50,
+          child: OutlinedButton(
+            onPressed: onPressed,
+            style: TextButton.styleFrom(
+                foregroundColor: Color.fromARGB(255, 2, 175, 255),
+                backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            child: Text(text),
+          ),
+        ),
+      ),
     );
   }
 
